91d0709506842850fceec4939efdd4e5f8083bdf
Laysan Nova <laysansh@gmail.com>
2025-04-06 16:47:40 -0600

    CI

--- conftest.py
+++ conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selenium import webdriver
 from utils.env_loader import load_env
-import os
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption("--env", action="store", default="dev", help="env name: dev/staging/prod")
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="Browser name: chrome or firefox"
+    )
+    parser.addoption(
+        "--env", action="store", default="dev", help="Environment name: dev/staging/prod"
+    )
 
 @pytest.fixture(scope="session", autouse=True)
 def load_environment(request):
@@ -12,7 +17,14 @@ def load_environment(request):
     load_env(env_name)
 
 @pytest.fixture
-def browser():
-    driver = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        chrome_options = Options()
+        chrome_options.add_argument("--log-level=3")
+        driver = webdriver.Chrome(options=chrome_options)
+
     yield driver
     driver.quit()

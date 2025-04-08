be7e3faacb5e2fe75c47554b665dc72f2e0c8abf
Laysan Nova <laysansh@gmail.com>
2025-04-07 19:38:16 -0600

    ci

--- conftest.py
+++ conftest.py
@@ -42,7 +42,7 @@ def browser(request):
     else:
         chrome_options = Options()
         chrome_options.add_argument("--log-level=3")
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         driver = webdriver.Chrome(options=chrome_options)
 
